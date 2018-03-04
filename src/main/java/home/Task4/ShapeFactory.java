e44bc7b6aa43a93fb4d05034c89f743c65149191
wolfchkov <wolfchkovaj@gmail.com>
2018-03-04 23:37:05 +0200

    Update ShapeFactory.java

--- src/main/java/home/Task4/ShapeFactory.java
+++ src/main/java/home/Task4/ShapeFactory.java
@@ -3,8 +3,13 @@ package home.Task4;
 import java.util.Scanner;
 
 public abstract class ShapeFactory {
+    //поле должено быть protected
     Scanner scan = new Scanner(System.in);
 
+    /**
+    Метод должен врзвращат Shape и должен быть абстрактным
+    а реализация в классе-фабрики конкретной фигуры
+    **/
     public int getShape(){
         return 0;
     }
