4171fe806856c0a52cf7e3146f96782be7210bb3
wolfchkov <wolfchkovaj@gmail.com>
2018-04-16 20:40:48 +0300

    Update Parser.java
    замечания

--- src/main/java/home/calculator/Parser.java
+++ src/main/java/home/calculator/Parser.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 
 public class Parser {
 
+    //почему имя метода с большой буквы?
     public String[] ExpressionParser () throws ParserException {
         String expression;
         System.out.println("Введите выражение");
         Scanner scan = new Scanner(System.in);
         expression = scan.nextLine();
+        /*
+        if (!expression.contains(" ")) {
+            throw new ParserException("Please use whitespace between your parameters");
+        }
+        Иправить
+        */
         if (!(expression.contains(" "))) throw new ParserException("Please use whitespace between your parameters");
         String [] expArr = expression.split(" ");
+        //исправить форматирование
         if (expArr.length != 3) throw new ParserException("Please make sure that you enter all needed parameters with whitespace as splitter");
         return expArr;
     }
