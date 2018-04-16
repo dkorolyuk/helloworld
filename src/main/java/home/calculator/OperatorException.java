6daa9320b59d59c0e72f18ba8cb1be7a7836351f
wolfchkov <wolfchkovaj@gmail.com>
2018-04-16 20:36:29 +0300

    Update OperatorException.java
    исправить

--- src/main/java/home/calculator/OperatorException.java
+++ src/main/java/home/calculator/OperatorException.java
@@ -1,7 +1,8 @@
 package home.calculator;
 
-public class OperatorException extends ParserException {
+public class OperatorException extends ParserException {    
     public void OperatorExeption() {
+        //ненадо ничего выводить в консоль в конструкторе исключений
         System.err.println("Unknown operator");
     }
 }
